61c44b496863d530c9d45e5d3b65f1093c5cf3af
MaseWaldrip <mtwaldri@mtu.edu>
2023-09-21 17:27:18 -0400

    Made Header Tab

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPage(pageName) {
+    if(pageName == 'home'){ window.location.href = "./index.html" }
+    if(pageName == 'experience'){ window.location.href = "./experience.html" }
+    if(pageName == 'sound'){ window.location.href = "./sound.html" }
+    if(pageName == 'broadcasting'){ window.location.href = "./broadcasting.html" }
+    if(pageName == 'visual_design'){ window.location.href = "./visual_design.html" }
+    if(pageName == 'contact'){ window.location.href = "./contact.html" }
+    return;
+}
